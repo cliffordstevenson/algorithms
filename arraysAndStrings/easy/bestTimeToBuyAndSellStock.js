e9dbcc328da5e750b20168b27453be27e3a1cf26
clifford-andrew-stevenson <clifford.andrew.stevenson@gmail.com>
2023-03-07 10:11:34 -0800

    changes

--- arraysAndStrings/easy/bestTimeToBuyAndSellStock.js
+++ arraysAndStrings/easy/bestTimeToBuyAndSellStock.js
@@ -21,56 +21,56 @@
 // Explanation: In this case, no transactions are done and the max profit = 0.
  
 
-// Constraints:
+Constraints:
 
-// 1 <= prices.length <= 105
-// 0 <= prices[i] <= 104
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
 
-// Solution 1
+Solution 1
 
-// We will use Two Pointers
+We will use Two Pointers
 
-// L = Buy
-// R = Sell
+L = Buy
+R = Sell
 
-// only writing one for loop, so time complexity = 0(N) linear
-// not using any extra space, so space complexity = 0(1) constant
+only writing one for loop, so time complexity = 0(N) linear
+not using any extra space, so space complexity = 0(1) constant
 
-// var maxProfit = function(prices) {
-//     let minBuyPrice= prices[0];
-//     let max=0;
-//     for(let i = 0; prices.length; i++){
-//         const sellPrice=prices[i]
-//         const profit=sellPrice-minBuyPrice
-//         max=Math.max(max, profit)
-//         // When would be the best time to buy?
-//         // given that we are to sell on dat 1
-//         minBuyPrice
-//     }
-//     return max;
-// };
+var maxProfit = function(prices) {
+    let minBuyPrice= prices[0];
+    let max=0;
+    for(let i = 0; prices.length; i++){
+        const sellPrice=prices[i]
+        const profit=sellPrice-minBuyPrice
+        max=Math.max(max, profit)
+        // When would be the best time to buy?
+        // given that we are to sell on dat 1
+        minBuyPrice
+    }
+    return max;
+};
 
 // Solution 2
 
-var maxProfit = function(prices) {
-    // Set Left Pointer, Min buy price to Infinity, because we
-    // want to set a number so high that wheneve we get to the first
-    // actual price, it's definitely lower than that (Infinity)
-    let minBuyPrice = Infinity;
-    // Right Pointer
-    let maxProfit = 0;
-    // For Loop
-    for (let price of prices) {
-    // price will symbolize whichever stock were on
-    // now we'll check is the price of whatever stock were on lower
-    // than our minimum buy price, well yes 7 is lower than infinity
-        if(price < minBuyPrice) {
-            minBuyPrice = price;
-        } else if(price - minBuyPrice > maxProfit) {
-            maxProfit = price - minBuyPrice;
-        } 
-    }
-    return maxProfit;
-}
+// var maxProfit = function(prices) {
+//     // Set Left Pointer, Min buy price to Infinity, because we
+//     // want to set a number so high that wheneve we get to the first
+//     // actual price, it's definitely lower than that (Infinity)
+//     let minBuyPrice = Infinity;
+//     // Right Pointer
+//     let maxProfit = 0;
+//     // For Loop
+//     for (let price of prices) {
+//     // price will symbolize whichever stock were on
+//     // now we'll check is the price of whatever stock were on lower
+//     // than our minimum buy price, well yes 7 is lower than infinity
+//         if(price < minBuyPrice) {
+//             minBuyPrice = price;
+//         } else if(price - minBuyPrice > maxProfit) {
+//             maxProfit = price - minBuyPrice;
+//         } 
+//     }
+//     return maxProfit;
+// }
 
 
