2b4a0f2118156e76ad3a64888f1451b9d1a90462
clifford-andrew-stevenson <clifford.andrew.stevenson@gmail.com>
2023-03-20 19:04:41 -0700

    validAnagramJava

---
+++ arraysAndStrings/easy/validAnagram.java
@@ -0,0 +1,22 @@
+public class validAnagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+    int[] counts = new int[26];
+    for(int i = 0; i < s.length(); i++) {
+        counts[s.charAt(i) - 'a']++;
+        counts[t.charAt(i) - 'a']--;
+    }
+
+    for(int i: counts) {
+        if(i != 0) {
+            return false;
+        }
+    }
+    return true;
+    }
+}
